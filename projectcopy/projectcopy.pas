64b89d67a47ffd45de7aab722650ea2d1b57e79f
lkrippahl <Cliente@.(none)>
2011-08-16 05:49:44 +0100

    Working for FPC projects
    Basic functionalities implemented.

---
+++ projectcopy/projectcopy.pas
@@ -0,0 +1,252 @@
+{*******************************************************************************
+                    This file is part of projectcopy
+
+This source file is public domain. It is provided as is, with no explicit
+or implied warranties regarding anything whatsoever.
+********************************************************************************
+Author: Ludwig Krippahl
+Date: 15.8.2011
+Purpose:
+  Copies the source files for the specified projects, along with all
+  unit files used by those projects (recursively) that are in the same
+  base folder.
+Revisions:
+TODO:
+  Ignore coments in FPC uses clauses
+  Add support for python projects
+
+*******************************************************************************}
+program projectcopy;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes, SysUtils, enumeratefolders, CustApp, basetypes, stringutils, fileutil
+  { you can add units after this };
+
+type
+
+  { TFPCProjectCopy }
+
+  TFPCProjectCopy = class(TCustomApplication)
+  protected
+    FSourceFolder,FDestFolder:string;
+    FFilesList:TEFFileTree;
+    FProjects:TSimpleStrings;
+    FAddedUnits:TSimpleStrings;
+    FAddedFiles:TSimpleStrings;
+    procedure DoRun; override;
+    procedure AddFPC;
+    procedure AddFPCUnit(PasFile:string);
+    function ListUsedFPCModules(Sl:TStringList):TSimpleStrings;
+    procedure CopyTree;
+  public
+    constructor Create(TheOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure WriteHelp; virtual;
+  end;
+
+{ TFPCProjectCopy }
+
+procedure TFPCProjectCopy.DoRun;
+var
+  ErrorMsg: String;
+  f:Integer;
+
+begin
+  // quick check parameters
+  ErrorMsg:=CheckOptions('h','help');
+  if (ErrorMsg<>'') or (ParamCount<3) or HasOption('h','help') then
+    begin
+    WriteHelp;
+    Terminate;
+    Exit;
+  end;
+
+  FAddedUnits:=nil;
+  FAddedFiles:=nil;
+  FSourceFolder:=ParamStr(1);
+  FDestFolder:=ParamStr(2);
+  SetLength(FProjects,ParamCount-2);
+  for f:=3 to ParamCount do
+    FProjects[f-3]:=ParamStr(f);
+  FFilesList:=TEFFileTree.Create(FSourceFolder);
+  AddFPC;
+  CopyTree;
+  // stop program loop
+  Terminate;
+end;
+
+procedure TFPCProjectCopy.AddFPC;
+
+var
+  f:integer;
+  tmpfile:string;
+
+procedure AddFile(FileName:string);
+
+var tmp:string;
+
+begin
+  tmp:=FFilesList.FindFile(FileName+'.pas');
+  if tmp<>'' then AddToArray(tmp,FAddedFiles);
+
+  tmp:=FFilesList.FindFile(FileName+'.res');
+  if tmp<>'' then AddToArray(tmp,FAddedFiles);
+
+  tmp:=FFilesList.FindFile(FileName+'.lpi');
+  if tmp<>'' then AddToArray(tmp,FAddedFiles);
+
+  tmp:=FFilesList.FindFile(FileName+'.lpr');
+  if tmp<>'' then AddToArray(tmp,FAddedFiles);
+
+  tmp:=FFilesList.FindFile(FileName+'.lfm');
+  if tmp<>'' then AddToArray(tmp,FAddedFiles);
+
+  tmp:=FFilesList.FindFile(FileName+'.ico');
+  if tmp<>'' then AddToArray(tmp,FAddedFiles);
+end;
+
+begin
+  for f:=0 to High(FProjects) do
+    begin
+    tmpfile:=FFilesList.FindFile(FProjects[f]+'.pas');
+    if tmpfile<>'' then
+      begin
+      AddFPCUnit(tmpfile);
+      AddFile(FProjects[f]);
+      end;
+    end;
+  for f:=0 to High(FAddedUnits) do
+    AddFile(FAddedUnits[f]);
+end;
+
+procedure TFPCProjectCopy.AddFPCUnit(PasFile:string);
+
+var
+  f:Integer;
+  tmpfile:string;
+  sl:TStringList;
+  units:TSimpleStrings;
+
+begin
+  sl:=TStringList.Create;
+  sl.LoadFromFile(PasFile);
+  units:=ListUsedFPCModules(sl);
+  for f:=0 to High(units) do
+    begin
+    if LastIndexOf(units[f],FAddedUnits)<0 then
+      begin
+      tmpfile:=FFilesList.FindFile(units[f]+'.pas');
+      if tmpfile<>'' then
+        begin
+        AddToArray(units[f],FAddedUnits);
+        AddFPCUnit(PasFile);
+        end;
+      end;
+    end;
+  sl.Free;
+end;
+
+function TFPCProjectCopy.ListUsedFPCModules(Sl: TStringList): TSimpleStrings;
+
+var
+  f:Integer;
+  tmp,line,pasfile:string;
+  units:TSimpleStrings;
+begin
+  f:=0;
+  line:='';
+  Result:=nil;
+  //there may be more than one uses clause
+  repeat
+    while (f<Sl.Count) and (Pos('USES',UpperCase(Sl.Strings[f]))<1) do
+      Inc(f);
+    if (f<Sl.Count) then
+      begin
+      tmp:=Sl.Strings[f];
+      Delete(tmp,1,Pos('USES',UpperCase(tmp))+3);
+      line:=line+tmp;
+      Inc(f);
+      while (f<Sl.Count) and (Pos(':',line)<1) do
+        begin
+        line:=line+Sl.Strings[f];
+        Inc(f);
+        end;
+      end;
+  until (f>=Sl.Count);
+  if line<>'' then
+    begin
+    line:=Deblank(line);
+    Result:=SplitString(line,',');
+    end;
+
+end;
+
+procedure TFPCProjectCopy.CopyTree;
+
+var
+  folders:TSimpleStrings;
+  f:Integer;
+  newfile:string;
+begin
+  FFilesList.ResetList(FAddedFiles);
+  folders:=FFilesList.ListFolders;
+  WriteLn('Folder Tree:');
+  for f:=0 to High(folders) do WriteLn(folders[f]);
+
+  for f:=0 to High(folders) do
+    if not DirectoryExists(FDestFolder+folders[f]) then
+      CreateDir(FDestFolder+folders[f]);
+
+  WriteLn('Files Copied:');
+  for f:=0 to High(FAddedFiles) do
+    begin
+    newfile:=FAddedFiles[f];
+    Delete(newfile,1,length(FSourceFolder));
+    newfile:=FDestFolder+newfile;
+    WriteLn(FAddedFiles[f],'->',newfile);
+    CopyFile(FAddedFiles[f],newfile);
+    end;
+end;
+
+constructor TFPCProjectCopy.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+  StopOnException:=True;
+end;
+
+destructor TFPCProjectCopy.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TFPCProjectCopy.WriteHelp;
+begin
+  { add your help code here }
+  WriteLn;
+  writeln('Use: ',ExeName,' source_folder destination_folder [project1] [project2] ...');
+  WriteLn;
+  writeln('Source and destination folders are required, followed by the names'+#10+
+          'of the project files to copy to destination (name only, no extension)'+#10+
+          'Will copy .pas .res .lpi .lpr .lrs .ico and .lfm files for these projects and'+#10+
+          'for all units in the dependency tree that are under the source folder,');
+  WriteLn('replicating the tree structure in the destination folder');
+  WriteLn('Example (Windows):');
+  WriteLn;
+end;
+
+var
+  Application: TFPCProjectCopy;
+
+{$R *.res}
+
+begin
+  Application:=TFPCProjectCopy.Create(nil);
+  Application.Run;
+  Application.Free;
+end.
+
---
+++ sharedunits/utils/enumeratefolders.pas
@@ -0,0 +1,190 @@
+{*******************************************************************************
+This source file is public domain. It is provided as is, with no explicit
+or implied warranties regarding anything whatsoever.
+********************************************************************************
+Author: Ludwig Krippahl
+Date: 15.8.2011
+Purpose:
+  Utilities and class for listing files in folder and subfolders
+Requirements:
+Revisions:
+To do:
+*******************************************************************************}
+
+unit enumeratefolders;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, basetypes, stringutils,quicksort;
+
+type
+  TEFCallback=procedure(Folder:string) of object;
+
+  { TEFFileTree }
+
+  TEFFileTree=class
+  protected
+    FBaseFolder:string;
+    FFiles:TSimpleStrings;
+    procedure FolderEnumCallback(Folder:string);
+  public
+    constructor Create(BaseFolder:string);
+    function ListByExtension(Ext:string;CaseSensitive:Boolean=False):TSimpleStrings;
+      //Ext includes the dot. E.g. '.bat'
+    function ListByFolder(Folder:string):TSimpleStrings;
+      //Folder is full path name if BaseFolder was too, otherwise local.
+      //TODO:Not Implemented
+    function ListFolders(ClipBaseFolder:Boolean=True;SortByLength:Boolean=True):TSimpleStrings;
+    function FindFile(FileName:string):string;
+      //returns first instance of filename
+      //NOTE: Case sensitive!
+      //TODO: wildcards
+    procedure ResetList(NewFiles:TSimpleStrings);
+  end;
+
+
+procedure EFEnumerate(Folder:string;Callback:TEFCallback);
+function EFListFiles(Folder:string):TSimpleStrings;
+
+
+implementation
+
+function EFListFiles(Folder:string):TSimpleStrings;
+
+var
+  srec:TSearchRec;
+
+begin
+  Result:=nil;
+  if FindFirst(Folder+PathDelim+'*.*',faAnyFile,srec)=0 then
+    repeat
+    if ((srec.Attr and faDirectory)=0) then
+      AddToArray(Folder+PathDelim+srec.Name,Result);
+    until FindNext(srec)<>0;
+  FindClose(srec);
+end;
+
+procedure EFEnumerate(Folder:string;Callback:TEFCallback);
+
+var
+  srec:TSearchRec;
+
+begin
+  if Assigned(Callback) then Callback(Folder);
+  if FindFirst(Folder+PathDelim+'*.*',faAnyFile,srec)=0 then
+    repeat
+    if  ((srec.Attr and faDirectory)=faDirectory) and
+      (srec.Name<>'.') and (srec.Name<>'..') then
+      EFEnumerate(Folder+PathDelim+srec.Name,Callback);
+    until FindNext(srec)<>0;
+  FindClose(srec);
+end;
+
+{ TEFFileTree }
+
+procedure TEFFileTree.FolderEnumCallback(Folder: string);
+
+begin
+  FFiles:=Concatenate(FFiles,EFListFiles(Folder));
+end;
+
+constructor TEFFileTree.Create(BaseFolder: string);
+begin
+  Assert(BaseFolder<>'','Invalid folder');
+  if BaseFolder[Length(BaseFolder)]=PathDelim then
+    Delete(BaseFolder,Length(BaseFolder),1);
+  inherited Create;
+  FBaseFolder:=BaseFolder;
+  EFEnumerate(BaseFolder,@FolderEnumCallback);
+end;
+
+function TEFFileTree.ListByExtension(Ext: string; CaseSensitive: Boolean=False): TSimpleStrings;
+
+var
+  fileext:string;
+  f:Integer;
+
+begin
+  Result:=nil;
+  if not CaseSensitive then
+    Ext:=UpperCase(Ext);
+  for f:=0 to High(FFiles) do
+    begin
+    fileext:=ExtractFileExt(FFiles[f]);
+    if not CaseSensitive then
+      fileext:=UpperCase(fileext);
+    if fileext=ext then
+      AddToArray(FFiles[f],Result);
+    end;
+end;
+
+function TEFFileTree.ListByFolder(Folder: string): TSimpleStrings;
+begin
+  Assert(False,'Not implemented'); //TODO: Implement...
+end;
+
+function TEFFileTree.ListFolders(ClipBaseFolder: Boolean; SortByLength: Boolean): TSimpleStrings;
+
+var
+  f:Integer;
+  oldfold,newfold:string;
+  tmpstrings:TSimpleStrings;
+  lens:TIntegers;
+begin
+  Result:=nil;
+  oldfold:='';
+  for f:=0 to High(FFiles) do
+    begin
+    newfold:=ExtractFileDir(FFiles[f]);
+    if newfold<>oldfold then
+      begin
+      oldfold:=newfold;
+      if ClipBaseFolder then Delete(newfold,1,Length(FBaseFolder));
+      repeat
+        if LastIndexOf(newfold,Result)<0 then
+            AddToArray(newfold,Result);
+         newfold:=ExtractFileDir(newfold);
+      until Length(newfold)<2;
+        //ususlly newfold is only the path delimiter
+        //TO DO:test this better
+      end;
+    end;
+  if SortByLength then
+    begin
+    SetLength(lens,Length(Result));
+    SetLength(tmpstrings,Length(Result));
+    for f:=0 to High(Result) do
+      lens[f]:=Length(Result[f]);
+    lens:=QSAscendingIndex(lens);
+    for f:=0 to High(lens) do
+      tmpstrings[f]:=Result[lens[f]];
+    Result:=tmpstrings;
+    end;
+end;
+
+function TEFFileTree.FindFile(FileName: string): string;
+
+var f,p:Integer;
+
+begin
+  Result:='';
+  for f:=0 to High(FFiles) do
+    if FileName=ExtractFileName(FFiles[f]) then
+      begin
+      Result:=FFiles[f];
+      Break;
+      end;
+end;
+
+procedure TEFFileTree.ResetList(NewFiles: TSimpleStrings);
+begin
+  FFiles:=Copy(NewFiles,0,Length(NewFiles));
+end;
+
+
+
+end.
+
